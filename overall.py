6a5f5dc4d7e2fc98a2c64a39e09b909a5fb1571f
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-05-04 07:21:28 -0500

    4

--- overall.py
+++ overall.py
@@ -1,44 +1,46 @@
-# TODO: Import modules
-def main():
-    cars = {}  # Dictionary to store cars with car_id as key and car objects as values
+from car_utils import create_car_from_input, display_cars
+from car import Car
 
-    while True:
-        print("\nMenu:")
-        print("1. Add a new car")
-        print("2. View all cars")
-        print("3. Drive a car")
-        print("4. Paint a car")
-        print("5. Exit")
+cars = {}
 
-        choice = input("Choose an option:\n")
+while True:
+    print("\nMenu:")
+    print("1. Add a new car")
+    print("2. View all cars")
+    print("3. Drive a car")
+    print("4. Paint a car")
+    print("5. Exit")
 
-        if choice == '1':
-          """TODO: Call the appropriate function from utils.py to create 
-          the car, add it to the dictionary, and print the car."""
+    choice = input("Enter your choice:\n")
 
+    if choice == "1":
+        # Crear carro
+        car = create_car_from_input()
+        cars[car.car_id] = car
+        print(car)
+        print("Car added.")
 
-        elif choice == '2':
-          """TODO: Call the appropriate function from utils.py to display
-          all the cars in the cars dictionary."""
+    elif choice == "2":
+        display_cars(cars)
 
-        elif choice == '3':
-          car_id = input("Enter the car ID to drive:\n")
-          miles = float(input("How many miles to drive?\n"))
-          """TODO: Look up the car in the dictionary, call the appropriate
-          class method to increase the mileage of the car, and print the car."""
-          
-        elif choice == '4':
-          car_id = input("Enter the car ID to paint:\n")
-          new_color = input("Enter the new color:\n")
-          """TODO: Look up the car in the dictionary, call the appropriate
-          class method to change the color of the car, and print the car."""
+    elif choice == "3":
+        car_id = input("Enter car ID:\n")
+        miles = float(input("Enter miles driven:\n"))
 
-        elif choice == '5':
-            print("Goodbye!")
-            break
+        if car_id in cars:
+            cars[car_id].drive(miles)
+            print("Mileage updated.")
+            print(cars[car_id])
 
-        else:
-            print("Invalid option. Please try again.")
+    elif choice == "4":
+        car_id = input("Enter car ID:\n")
+        new_color = input("Enter new color:\n")
 
-if __name__ == "__main__":
-    main()
+        if car_id in cars:
+            cars[car_id].change_color(new_color)
+            print("Color updated.")
+            print(cars[car_id])
+
+    elif choice == "5":
+        print("Goodbye!")
+        break
