e1ba50b8892ce1a1d9d8504861c8f6de90ded3c4
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-05-04 12:49:25 -0500

    9

--- utils.py
+++ utils.py
@@ -1,1 +1,20 @@
 # utils.py
+from person import Person
+from bank_account import BankAccount
+
+def person_data():
+    name = input("Enter the person's name:\n")
+    person = Person(name)
+
+    while True:
+        acc_num = int(input("Enter a 4-digit account number:\n"))
+        balance = float(input("Enter the initial balance:\n"))
+
+        account = BankAccount(acc_num, balance)
+        person.add_account(account)
+
+        done = input("Are you done adding accounts? (yes/no):\n")
+        if done == "yes":
+            break
+
+    return person
