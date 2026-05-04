f04f767fee57c779a2f4a50bdcfc83ba219eccaa
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-05-04 06:54:44 -0500

    3

--- song.py
+++ song.py
@@ -1,5 +1,13 @@
 # Write your code here!
 
-class song:
-    def __init__(self):
-        return 
+class Song:
+    def __init__(self, name, artist, length):
+        self.name = name
+        self.artist = artist
+        self.length = length
+
+    def get_length_in_seconds(self):
+        return self.length * 60
+
+    def __str__(self):
+        return f"'{self.name}' by {self.artist} ({self.length})"
