38cf28b2f0c9bd40e1cbc1cc11725db53bcdd749
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-05-04 19:06:47 -0500

    25

--- aircraft_altitude.py
+++ aircraft_altitude.py
@@ -1,21 +1,21 @@
 from aircraft import Aircraft
 
-model = input("Enter aircraft model:\n")
-plane = Aircraft(model)
+if __name__ == "__main__":
+    model = input()
+    aircraft = Aircraft(model)
 
-while True:
-    command = input("Enter command (A for ascent, D for descent, X to exit):\n")
+    while True:
+        command = input()
 
-    if command == "X":
-        break
+        if command == "X":
+            break
 
-    parts = command.split()
-    action = parts[0]
-    feet = int(parts[1])
+        action, value = command.split()
+        value = int(value)
 
-    if action == "A":
-        plane.ascend(feet)
-    elif action == "D":
-        plane.descend(feet)
+        if action == "A":
+            aircraft.climb(value)
+        elif action == "D":
+            aircraft.descend(value)
 
-print(f"Final altitude: {plane.altitude} feet")
+    print(aircraft.altitude)
